d0b500e0ef0ac8accbbfc77503b6a028f3adab27
nurshaandynr <cindyy@student.telkomuniversity.ac.id>
2022-11-05 08:05:46 +0700

    MODUL 1

---
+++ MODUL 1/Server.java
@@ -0,0 +1,23 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        scanner input = new scanner(System.in);
+
+        System.out.println("Selamat datang");
+        System.out.println("Silahkan Register");
+        // TODO Create Database
+
+        // TODO Create Menu
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
